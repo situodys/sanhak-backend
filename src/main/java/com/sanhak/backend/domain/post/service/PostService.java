30ad9ce247247a3df488ba97eb3d19130a7ec6a0
situodys <situodys@gmail.com>
2022-08-03 15:11:41 +0900

    Feat: 변경 findPostDetailByPostId

--- src/main/java/com/sanhak/backend/domain/post/service/PostService.java
+++ src/main/java/com/sanhak/backend/domain/post/service/PostService.java
@@ -2,6 +2,7 @@ package com.sanhak.backend.domain.post.service;
 
 import com.sanhak.backend.domain.post.Post;
 import com.sanhak.backend.domain.post.dto.PostDTO;
+import com.sanhak.backend.domain.post.dto.PostDetailDTO;
 import com.sanhak.backend.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -23,11 +24,11 @@ public class PostService {
                 .map(post -> modelMapper.map(post, PostDTO.class));
     }
 
-    public PostDTO findById(Long id) {
+    public PostDetailDTO findPostDetailByPostId(Long id) {
         Post post = postRepository
-                .findByPostId(id)
+                .findPostDetailByPostId(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당하는 post가 없습니다."));
-        return modelMapper.map(post, PostDTO.class);
+        return modelMapper.map(post, PostDetailDTO.class);
     }
 
     public void removeById(Long id) {
