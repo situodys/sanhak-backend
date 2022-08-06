587aff7577b3ed07fe5b1e386879c05c9973e1af
situodys <situodys@gmail.com>
2022-08-06 13:43:30 +0900

    Feat: 추가 getPageList api in post/repository
    - 리팩토링 고민 필요
    - 제목, 내용에 대해 키워드 검색 & 여러 정렬 처리 가능

--- src/main/java/com/sanhak/backend/domain/post/repository/PostRepositoryCustom.java
+++ src/main/java/com/sanhak/backend/domain/post/repository/PostRepositoryCustom.java
@@ -1,9 +1,13 @@
 package com.sanhak.backend.domain.post.repository;
 
 import com.sanhak.backend.domain.post.Post;
+import com.sanhak.backend.global.PageRequestDTO;
+import org.springframework.data.domain.PageImpl;
 
 import java.util.Optional;
 
 public interface PostRepositoryCustom {
     Optional<Post> findPostDetailByPostId(Long id);
+
+    PageImpl<Object[]> getPostList(PageRequestDTO pageRequestDTO);
 }
--- src/main/java/com/sanhak/backend/domain/post/repository/PostRepositoryImpl.java
+++ src/main/java/com/sanhak/backend/domain/post/repository/PostRepositoryImpl.java
@@ -1,10 +1,25 @@
 package com.sanhak.backend.domain.post.repository;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sanhak.backend.domain.post.Post;
+import com.sanhak.backend.global.PageRequestDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import static com.sanhak.backend.domain.comment.QComment.comment;
 import static com.sanhak.backend.domain.post.QPost.post;
 
@@ -26,5 +41,101 @@ public class PostRepositoryImpl implements PostRepositoryCustom{
                 .fetchOne();
 
         return Optional.ofNullable(result);
+    }
+
+    @Override
+    public PageImpl<Object[]> getPostList(PageRequestDTO pageRequestDTO) {
+
+        List<Long> ids;
+        int totalCount= pageRequestDTO.getTotalCount();
+
+        if (totalCount == -1) {
+            totalCount = getTotalListCount(pageRequestDTO);
+        }
+
+        List<OrderSpecifier> orders = getOrderSpecifier(pageRequestDTO.getSort());
+        ids = getPostListIds(pageRequestDTO,orders);
+        if (ids.isEmpty()) {
+            return null;
+        }
+
+
+        List<Tuple>  result = jpaQueryFactory.select(post.id,post.title,post.content, comment.count())
+                .from(post)
+                .leftJoin(comment).on(comment.post.eq(post))
+                .where(
+                        post.id.in(ids)
+                )
+                .orderBy(orders.stream().toArray(OrderSpecifier[]::new))
+                .groupBy(post)
+                .fetch();
+
+        return new PageImpl<Object[]>(
+                result.stream()
+                        .map(tuple -> tuple.toArray())
+                        .collect(Collectors.toList()), pageRequestDTO.getPageable(), totalCount
+        );
+
+
+    }
+
+    private List<OrderSpecifier> getOrderSpecifier(Sort sort) {
+        List<OrderSpecifier> orders = new ArrayList<>();
+
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+            String prop = order.getProperty();
+            PathBuilder orderExpression = new PathBuilder(Post.class, "post");
+            orders.add(new OrderSpecifier(direction, orderExpression.get(prop)));
+        });
+        return orders;
+    }
+
+    private int getTotalListCount(PageRequestDTO pageRequestDTO) {
+        String searchType = pageRequestDTO.getSearchType();
+        String searchKeyword = pageRequestDTO.getSearchKeyword();
+
+
+        return jpaQueryFactory.select(post.id)
+                .from(post)
+                .where(
+                        post.id.gt(0L)
+                        ,isTitleContainsKeyword(searchType, searchKeyword)
+                        ,isContentContainsKeyword(searchType, searchKeyword)
+                )
+                .fetch().size();
+    }
+
+    private List<Long> getPostListIds(PageRequestDTO pageRequestDTO, List<OrderSpecifier> orders) {
+        String searchType = pageRequestDTO.getSearchType();
+        String searchKeyword = pageRequestDTO.getSearchKeyword();
+
+
+        return jpaQueryFactory.select(post.id)
+                .from(post)
+                .where(
+                        post.id.gt(0L)
+                        , isTitleContainsKeyword(searchType, searchKeyword)
+                        , isContentContainsKeyword(searchType, searchKeyword)
+                )
+                .offset(pageRequestDTO.getOffset())
+                .limit(pageRequestDTO.getSize())
+                .orderBy(orders.stream().toArray(OrderSpecifier[]::new))
+                .fetch();
+
+    }
+
+    private BooleanExpression isTitleContainsKeyword(String searchType,String searchKeyword) {
+        if (searchType==null || !searchType.contains("t")) {
+            return null;
+        }
+        return post.title.containsIgnoreCase(searchKeyword);
+    }
+
+    private BooleanExpression isContentContainsKeyword(String searchType,String searchKeyword) {
+        if (searchType==null || !searchType.contains("c")) {
+            return null;
+        }
+        return post.content.containsIgnoreCase(searchKeyword);
     }
 }
