915867e236b7b879cf7bd88f735da0e53867967c
Hukka <rhlehfndvkd7557@daum.net>
2022-08-07 21:44:44 +0900

    fix : post pagination using findAll

--- src/main/java/com/sanhak/backend/domain/post/controller/PostController.java
+++ src/main/java/com/sanhak/backend/domain/post/controller/PostController.java
@@ -5,11 +5,8 @@ import com.sanhak.backend.domain.post.dto.PostDetailDTO;
 import com.sanhak.backend.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/post")
@@ -18,14 +15,17 @@ public class PostController {
 
     private final PostService postService;
 
-    // pagination
     @GetMapping("/")
-    public Page<PostDTO> findByPagination(Pageable pageable){
-        return postService.findByPagination(pageable);
+    public Page<PostDTO> findByPagination(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size
+    ) {
+        PageRequest pr = PageRequest.of(page, size);
+        return postService.findByPagination(pr);
     }
 
     @GetMapping("/detail/{id}")
-    public PostDetailDTO findById(@PathVariable("id") Long id){
+    public PostDetailDTO findById(@PathVariable("id") Long id) {
         return postService.findPostDetailByPostId(id);
     }
 
